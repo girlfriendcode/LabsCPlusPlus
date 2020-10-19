9e294a23db7e812b73d5c9538b193d1a06e0b023
Valeria Bushueva <62572271+girlfriendcode@users.noreply.github.com>
2020-10-19 12:32:48 +0300

    Create HexNumber.h

---
+++ lab3/HexNumber.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+namespace Prog3 {
+	class HexNumber
+	{
+	private:
+		static const int maxLen = 32;
+		char hex[maxLen];
+		int length;//фактическая длина
+	public:
+		HexNumber();
+		HexNumber(char* str);
+		HexNumber(const long int a);
+		HexNumber(const HexNumber& number);
+    
+		char getSign()const;
+		char getElement(int i)const;
+    
+		std::ostream& output(std::ostream& o);
+		std::istream& input(std::istream& i);
+    
+		HexNumber& setN(char* str);
+    HexNumber& Convert();
+		const HexNumber Add(const HexNumber& num);
+		const HexNumber Sub(const HexNumber& num);
+    
+		HexNumber& moveRight(int a);
+		HexNumber& moveLeft(int a);
+		
+		char Compare(const HexNumber& num);//сравнение
+		int Parity();//чётность
+	};
+	void out(HexNumber&, HexNumber&, HexNumber&, const char*);
+  void dialog(HexNumber&, HexNumber&);
+  
+	char upper(char num);
+  
+	int CharToHex(char num);
+	char HexToChar(int num);
+	
+}
+
+
