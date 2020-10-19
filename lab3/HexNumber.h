c4b10e70aad181cb4cc4ef2ef78754ac6682922b
Valeria Bushueva <62572271+girlfriendcode@users.noreply.github.com>
2020-10-20 00:51:57 +0300

    Update HexNumber.h

--- lab3/HexNumber.h
+++ lab3/HexNumber.h
@@ -1,28 +1,30 @@
-#pragma once
+#include <iosfwd>
+#ifndef __HEX_NUMBER_H__
+#define __HEX_NUMBER_H__
 #include <iostream>
 namespace Prog3 {
 	class HexNumber
 	{
 	private:
-		static const int maxLen = 32;
-		char hex[maxLen];
+		static const int MAX_LEN = 32;
+		char hex[MAX_LEN];
 		int length;//длина введённого пользователем символьного массива
 	public:
 		HexNumber();
 		HexNumber(char* str);
-		HexNumber(const long int a);
+		HexNumber(const long  a);
 		HexNumber(const HexNumber& number);
     
-		char getSign()const;
-		char getElement(int i)const;
+		char getSign()const { return hex[0] ;};
+		char getElement(int i)const { return hex[i] ;};
     
-		std::ostream& output(std::ostream& o);
+		std::ostream& output(std::ostream& o) const;
 		std::istream& input(std::istream& i);
     
 		HexNumber& setN(char* str);
-    HexNumber& Convert();
-		const HexNumber Add(const HexNumber& num);
-		const HexNumber Sub(const HexNumber& num);
+                HexNumber& Convert();
+	        HexNumber& Add(const HexNumber& num);
+		HexNumber& Sub(const HexNumber& num);
     
 		HexNumber& moveRight(int a);
 		HexNumber& moveLeft(int a);
@@ -30,14 +32,11 @@ namespace Prog3 {
 		char Compare(const HexNumber& num);//сравнение
 		int Parity();//чётность
 	};
-	void out(HexNumber&, HexNumber&, HexNumber&, const char*);
-  void dialog(HexNumber&, HexNumber&);
-  
-	char upper(char num);
+	void out(HexNumber& op1, HexNumber& op2, HexNumber& res, const char*);
   
 	int CharToHex(char num);
 	char HexToChar(int num);
 	
 }
-
+#endif /*!__HEX_NUMBER_H */
 
